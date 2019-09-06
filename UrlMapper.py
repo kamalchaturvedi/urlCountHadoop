55ae032b5b6cd093966d940b7e051bdcc6e6246d
Kamal Chaturvedi <kamalchaturvedi15@gmail.com>
2019-09-06 09:21:49 -0600

    Made urlcount changes to py files

---
+++ UrlMapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+import re
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', word)
+        if len(url) > 0:
+            print('%s\t%s' % (url[0], 1))
